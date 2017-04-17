8e68d5aad4d841023b3200e3370cf9b9a6f55ed7
Fran Guijarro <franleplant@gmail.com>
2017-04-17 19:55:33 -0300

    improves to ergonomics and performance

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,14 @@
+use std::rc::Rc;
 
-
-#[derive(Debug, Clone)]
-struct Node {
-    data: u32,
-    left: Option<Box<Node>>,
-    right: Option<Box<Node>>,
+#[derive(Debug)]
+pub struct Node {
+    pub data: u32,
+    pub left: Option<Rc<Node>>,
+    pub right: Option<Rc<Node>>,
 }
 
 impl Node {
-    fn new(data: u32) -> Node {
+    pub fn new(data: u32) -> Node {
         Node {
             data: data,
             left: None,
@@ -16,36 +16,33 @@ impl Node {
         }
     }
 
-    fn left(mut self, node: Box<Node>) -> Node {
+    pub fn left(mut self, node: Rc<Node>) -> Node {
         self.left = Some(node);
         self
     }
 
-    fn right(mut self, node: Box<Node>) -> Node {
+    pub fn right(mut self, node: Rc<Node>) -> Node {
         self.right = Some(node);
         self
     }
 
-    fn to_box(self) -> Box<Node> {
-        Box::new(self)
+    pub fn to_ptr(self) -> Rc<Node> {
+        Rc::new(self)
     }
 }
 
 enum NodeOrValue {
-    Node(Box<Node>),
+    Node(Rc<Node>),
     Value(u32),
 }
 
-//TODO
-//- avoid cloning
-fn print_in_order(node: Box<Node>) {
+pub fn print_in_order(node: Rc<Node>) {
     let mut stack: Vec<NodeOrValue> = vec![NodeOrValue::Node(node)];
 
-    while stack.len() != 0 {
-        let top = stack.pop().expect("Poping an empty stack");
+    while let Some(top) = stack.pop() {
+        //println!("Iteration");
         match top {
             NodeOrValue::Node(node) => {
-
                 if let &Some(ref right) = &node.right {
                     stack.push(NodeOrValue::Node(right.clone()));
                 }
@@ -67,55 +64,23 @@ fn print_in_order(node: Box<Node>) {
 
 #[test]
 fn it_works() {
-    let tree = Box::new(Node {
-        data: 5,
-        left: Some(Box::new(Node {
-            data: 3,
-            left: Some(Box::new(Node {
-                data: 2,
-                left: None,
-                right: None,
-            })),
-            right: Some(Box::new(Node {
-                data: 4,
-                left: None,
-                right: None,
-            })),
-        })),
-        right: Some(Box::new(Node {
-            data: 7,
-            left: Some(Box::new(Node {
-                data: 6,
-                left: None,
-                right: None,
-            })),
-            right: Some(Box::new(Node {
-                data: 8,
-                left: None,
-                right: None,
-            })),
-        })),
-    });
-
-    let tree2 = Node::new(5)
+    let tree = Node::new(5)
         .left(
             Node::new(3)
-                .left(Node::new(2).to_box())
-                .right(Node::new(4).to_box())
-                .to_box()
+                .left(Node::new(2).to_ptr())
+                .right(Node::new(4).to_ptr())
+                .to_ptr()
         )
         .right(
             Node::new(7)
-                .left(Node::new(6).to_box())
-                .right(Node::new(8).to_box())
-                .to_box()
+                .left(Node::new(6).to_ptr())
+                .right(Node::new(8).to_ptr())
+                .to_ptr()
         )
-        .to_box();
+        .to_ptr();
 
     println!("{:?}", tree);
-
+    println!("Ordered lineal print");
     print_in_order(tree);
 
-    println!("-----");
-    print_in_order(tree2);
 }
